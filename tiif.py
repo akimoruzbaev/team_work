a07dfc0e0644b0a1cfb3c481d67f5a100076498c
akimoruzbaev <80908496+akimoruzbaev@users.noreply.github.com>
2021-03-20 21:41:24 +0600

    Create tiif.py

---
+++ tiif.py
@@ -0,0 +1,11 @@
+class Car:
+    def __init__(self, name, year, owner):
+        self.name = name
+        self.year = year
+        self.owner = owner
+
+    def get_info(self):
+        print(f"Автомобиль {name}")
+        print(f"Год выпуска {year}")
+        print(f"Владелец {owner}")
+
